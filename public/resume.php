f600f5635ac697e0e5aaa92466a2b6a4fb854adf
kschat <kyleschattler@gmail.com>
2013-03-04 18:02:50 -0500

    Reformated resume and fixed various mistakes.

--- public/resume.php
+++ public/resume.php
@@ -10,92 +10,66 @@
 			<div class="resume-indent">
 				<h4 class="resume-header">Bachelors of Software Engineering</h4>
 				<ul class="resume-list resume-indent">
-					<li>One College Avenue, Williamsport, PA 17701</li>
-					<li>Completed third year.</li>
 					<li>Graduation GPA: 3.47/4.0</li>
 					<li>Major GPA: 3.68/4.0</li>
 				</ul>
 			</div>
 		</div>
 
-		<h2 class="resume-header">Experience</h3>
+		<h2 class="resume-header">Professional Experience</h3>
 		<div class="resume-indent">
+			<p>Various Clients (5/12 - 2/13)</p>
 			<h3 class="resume-header">Freelance Web Developer</h3>
 			<div class="resume-indent">
-				<h4 class="resume-header">December 2012 - February 2013</h4>
-				<p class="resume-indent">
-					Developed front and back end of a legal document annotation social network. PHP and Yii were used as the server side development environment. Bootstrap, 
-					Backbone, Require, and jQuery were used on the front end.
-				</p>
-			</div>
-		</div><br />
+				<ul class="resume-list bulleted">
+					<li class="resume-indent">
+						Developed front and back end of a legal document annotation social network. PHP and Yii were used as the server side development environment. 
+						Bootstrap, Backbone, Require, and jQuery were used on the front end.
+					</li>
 
-		<div class="resume-indent">
-			<h3 class="resume-header">Front and Back End Web Developer</h3>
-			<div class="resume-indent">
-				<h4 class="resume-header"><a href="http://www.blacknaps.org" target="_blank">Black Naps</a><br />May 2012 - October 2012</h4>
-				<p class="resume-indent">
-					Designed and developed a WordPress image gallery pluggin for blacknaps.org. was responsible for developing all parts of the web app including: JavaScript, jQuery, 
-					CSS3, and PHP. 
-				</p>
+					<li class="resume-indent">
+						Designed and developed a WordPress image gallery plugin for <a href="http://www.blacknaps.org" target="_blank">blacknaps.org</a>. PHP was 
+						used on the back end. JavaScript, HTML5, and CSS were used on the front end.
+					</li>
 			</div>
 		</div>
 
 		<h2 class="resume-header">Projects</h2>
 		<div class="resume-indent">
-			<h3 class="resume-header">Scheduler</h3>
+			<h3 class="resume-header">Scheduler (<a href="https://github.com/kschat/scheduler" target="_blank">GitHub</a>)</h3>
 			<p class="resume-indent">
-				An easier way to scheduler classes at Pennsylvania College of Technology. Developed using Node and Express for the server side environment and REST API, MongoDB 
-				as the database with Mongoose as the middleware to interface with the database and create models, Jade as the template engine, and Mocha along with Should for 
-				testing and assertion frameworks. The project is currently on <a href="https://github.com/kschat/Scheduler">github</a> to keep track of versions and 
-				allow for easy team development in the future.
-			</p>
-			
-			<h3 class="resume-header">Java Installer API</h3>
-			<p class="resume-indent">
-				Designed, developed, and tested a Java installer API to allow developers to easily create a native installer for their application. The program is being developed
-				with all operating systems in mind to allow for cross platform support. The project is currently on <a href="https://github.com/kschat/Java-Installer-API">github</a>
-				to keep track of versions and allow for easy team development in the future.
+				Created a simple way to schedule classes using Node, Express, MongoDB, Mongoose, and Jade. Wrote tests using Mocha with Should.
 			</p>
 
-			<h3 class="resume-header">TF2 Outpost Price Integration</h3>
+			<h3 class="resume-header">TF2 Outpost Price Integration (<a href="https://github.com/kschat/tf2outpost-prices-chrome-extension" target="_blank">GitHub</a>, <a href="https://chrome.google.com/webstore/detail/tf2outpost-price-integrat/ndflpbfgklcjpkepbmfgjjancaocpnmp" target="_blank">Download</a>)</h3>
 			<p class="resume-indent">
-				A Chrome extension developed using JavaScript. A few frameworks were also used to help with various aspects of the extension; these frameworks being require.js 
-				to help with structuring modules and load only what is necessary, backbone.js to help structure the extension into a MV* pattern, and jQuery to abstract browser 
-				specific DOM manipulations. The project is currently on <a href="https://github.com/kschat/tf2outpost-prices-chrome-extension">github</a>
-				to keep track of versions and allow for easy team development in the future.
+				Created a Chrome extension that pulls prices from backpack.tf and injects them in TF2 outpost using Require.js, Backbone.js, and jQuery.
 			</p>
 		</div>
 
 		<h2 class="resume-header">Technical Skills</h2>
 		<div class="resume-indent">
-			<h3 class="resume-header">Operating Systems</h3>
-			<p class="resume-indent">Windows (XP, Vista, 7, & 8), OS X (10.6 – 10.7), Linux (Debian based)</p>
 
 			<h3 class="resume-header">Programming</h3>
-			<p class="resume-indent">Java, C++, JavaScript, Node, CSS, HTML, MySQL, MongoDB, PHP</p>
-
-			<h3 class="resume-header">Frameworks</h3>
 			<p class="resume-indent">
-				JavaScript - Express, Require, Backbone, Underscore, jQuery, Mongoose<br />
-				PHP - Yii<br />
-				CSS - Bootstrap<br />
+				Java, C++, JavaScript, Node, CSS, HTML, MySQL, MongoDB, PHP, Express.js, Require.js, Backbone.js, Underscore.js, jQuery, 
+				Mongoose ODM, Yii, Bootstrap
 			</p>
 
-			<h3 class="resume-header">Tools</h3>
-			<p class="resume-indent">Git, Putty, CoreFTP, Fetch, Sublime Text 2, Eclipse, VirtualBox, TeamViewer, DropBox, WAMP/MAMP, PHPmyAdmin, and Apache</p>
+			<h3 class="resume-header">Software</h3>
+			<p class="resume-indent">
+				Windows, OS X, Linux (Debian based), Git, SVN, SSH, FTP, npm, curl, Eclipse, VirtualBox, Apache
+			</p>
 		</div>
 
 		<h2 class="resume-header">Activites and Achievements</h2>
 		<div class="resume-indent">
-			<ul class="resume-list">
+			<ul class="resume-list bulleted">
 				<li>Achieved Dean's List for Spring 2011, Fall 2011, Spring 2012, and Fall 2012 semesters</li>
-				<li>Member of the Penn College Programming Club</li>
-				<li>Member of the Penn College Security Club</li>
-				<li>Member of the Penn College Association for Computer Machinery</li>
+				<li>Member of the Penn College Security Club, Programming Club, and ACM</li>
 			</ul>
 		</div>
 		<br />
-		<a style="float: right; font-size: 16px;" href="http://www.kyleschattler.com/Resume.doc">Download copy</a>
+		<h3 style="text-align: center;"><a href="http://www.kyleschattler.com/Resume.doc">Download copy</a></h3>
 	</div>
 </div>
